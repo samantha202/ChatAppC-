69f5a825933956c4c233f13f1612587a050f7da1
samantha <nsamanthaduclaire@gmail.com>
2021-10-26 15:01:59 +0200

    SeverrForm interface Designed

--- ChatApp/Form1.Designer.cs
+++ ChatApp/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ChatApp
 {
-    partial class Form1
+    partial class Server
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,13 +29,168 @@ namespace ChatApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.Private = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.inputPrivate = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.Location = new System.Drawing.Point(26, 235);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(501, 525);
+            this.textBox1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(668, 302);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(331, 228);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(486, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 33);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Server";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(195, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 33);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Chat History";
+            this.label2.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label3.Location = new System.Drawing.Point(668, 235);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(270, 33);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Clients Connected";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStart.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnStart.Location = new System.Drawing.Point(934, 102);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(229, 59);
+            this.btnStart.TabIndex = 6;
+            this.btnStart.Text = "Start Server";
+            this.btnStart.UseVisualStyleBackColor = false;
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnStop.ForeColor = System.Drawing.SystemColors.Window;
+            this.btnStop.Location = new System.Drawing.Point(641, 102);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(229, 59);
+            this.btnStop.TabIndex = 7;
+            this.btnStop.Text = "Stop Server";
+            this.btnStop.UseVisualStyleBackColor = false;
+            // 
+            // Private
+            // 
+            this.Private.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.Private.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Private.ForeColor = System.Drawing.SystemColors.Window;
+            this.Private.Location = new System.Drawing.Point(961, 731);
+            this.Private.Name = "Private";
+            this.Private.Size = new System.Drawing.Size(186, 59);
+            this.Private.TabIndex = 8;
+            this.Private.Text = "Private";
+            this.Private.UseVisualStyleBackColor = false;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(615, 780);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(323, 10);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // inputPrivate
+            // 
+            this.inputPrivate.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.inputPrivate.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.inputPrivate.Location = new System.Drawing.Point(615, 707);
+            this.inputPrivate.Multiline = true;
+            this.inputPrivate.Name = "inputPrivate";
+            this.inputPrivate.Size = new System.Drawing.Size(323, 67);
+            this.inputPrivate.TabIndex = 10;
+            // 
+            // Server
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(1175, 1005);
+            this.Controls.Add(this.inputPrivate);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.Private);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Server";
+            this.Text = "Server";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button Private;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TextBox inputPrivate;
     }
 }
 
