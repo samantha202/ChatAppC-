2cbe5523453b9115b67ce0ca62736f0ef32b025e
samantha <nsamanthaduclaire@gmail.com>
2021-10-26 15:39:21 +0200

    ByteArrayToObject function overloaded

--- ChatApp/Form1.cs
+++ ChatApp/Form1.cs
@@ -90,5 +90,16 @@ namespace ChatApp
                 textBox1.AppendText(">>" + m + Environment.NewLine);
             });
         }
+        public Object ByteArrayToObject(byte[] arrBytes)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                var binForm = new BinaryFormatter();
+                memStream.Write(arrBytes, 0, arrBytes.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                var obj = binForm.Deserialize(memStream);
+                return obj;
+            }
+        }
     }
 }
