a82bc615a5c395ddc38bd58d9cacbed2d04b5798
samantha <nsamanthaduclaire@gmail.com>
2021-11-01 12:22:06 +0100

    main conversation interface designed (2)

--- Client/FormMain.Designer.cs
+++ Client/FormMain.Designer.cs
@@ -33,44 +33,41 @@ namespace Client
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.label1 = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.indicator = new System.Windows.Forms.Panel();
             this.input = new System.Windows.Forms.TextBox();
             this.btnSend = new System.Windows.Forms.Button();
             this.btnConnect = new System.Windows.Forms.Button();
-            this.indicator = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // history
             // 
-            this.history.BackColor = System.Drawing.SystemColors.HighlightText;
-            this.history.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.history.Location = new System.Drawing.Point(12, 105);
+            this.history.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.history.Location = new System.Drawing.Point(12, 68);
             this.history.Multiline = true;
             this.history.Name = "history";
             this.history.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.history.Size = new System.Drawing.Size(696, 380);
+            this.history.Size = new System.Drawing.Size(629, 444);
             this.history.TabIndex = 0;
-            this.history.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // listBox1
             // 
-            this.listBox1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 27;
-            this.listBox1.Location = new System.Drawing.Point(731, 103);
+            this.listBox1.ItemHeight = 32;
+            this.listBox1.Location = new System.Drawing.Point(696, 68);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(231, 382);
+            this.listBox1.Size = new System.Drawing.Size(240, 452);
             this.listBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(746, 50);
+            this.label1.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(730, 33);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(202, 37);
+            this.label1.Size = new System.Drawing.Size(171, 32);
             this.label1.TabIndex = 2;
             this.label1.Text = "Participants";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // tableLayoutPanel1
             // 
@@ -78,86 +75,74 @@ namespace Client
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(27, 664);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(32, 668);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(426, 8);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(355, 10);
             this.tableLayoutPanel1.TabIndex = 3;
             // 
-            // textBox2
+            // indicator
             // 
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Location = new System.Drawing.Point(1000, 800);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(420, 52);
-            this.textBox2.TabIndex = 4;
+            this.indicator.Location = new System.Drawing.Point(12, 28);
+            this.indicator.Name = "indicator";
+            this.indicator.Size = new System.Drawing.Size(28, 27);
+            this.indicator.TabIndex = 4;
             // 
             // input
             // 
             this.input.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.input.Location = new System.Drawing.Point(27, 590);
+            this.input.Font = new System.Drawing.Font("Arial", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.input.Location = new System.Drawing.Point(32, 597);
             this.input.Multiline = true;
             this.input.Name = "input";
-            this.input.Size = new System.Drawing.Size(426, 68);
+            this.input.Size = new System.Drawing.Size(355, 65);
             this.input.TabIndex = 5;
-            this.input.TextChanged += new System.EventHandler(this.input_TextChanged);
+            this.input.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // btnSend
             // 
             this.btnSend.BackColor = System.Drawing.SystemColors.HotTrack;
             this.btnSend.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnSend.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnSend.Location = new System.Drawing.Point(502, 626);
+            this.btnSend.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnSend.Location = new System.Drawing.Point(450, 648);
             this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(169, 56);
+            this.btnSend.Size = new System.Drawing.Size(178, 69);
             this.btnSend.TabIndex = 6;
             this.btnSend.Text = "Send";
             this.btnSend.UseVisualStyleBackColor = false;
-            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // btnConnect
             // 
             this.btnConnect.BackColor = System.Drawing.SystemColors.HotTrack;
             this.btnConnect.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.btnConnect.ForeColor = System.Drawing.SystemColors.ControlLightLight;
-            this.btnConnect.Location = new System.Drawing.Point(720, 626);
+            this.btnConnect.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.btnConnect.Location = new System.Drawing.Point(705, 648);
             this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(169, 56);
+            this.btnConnect.Size = new System.Drawing.Size(178, 69);
             this.btnConnect.TabIndex = 7;
             this.btnConnect.Text = "Connect";
             this.btnConnect.UseVisualStyleBackColor = false;
-            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
-            // 
-            // indicator
-            // 
-            this.indicator.Location = new System.Drawing.Point(12, 59);
-            this.indicator.Name = "indicator";
-            this.indicator.Size = new System.Drawing.Size(31, 28);
-            this.indicator.TabIndex = 8;
             // 
             // FormMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 27F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(974, 729);
-            this.Controls.Add(this.indicator);
             this.Controls.Add(this.btnConnect);
             this.Controls.Add(this.btnSend);
             this.Controls.Add(this.input);
-            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.indicator);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.history);
-            this.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "FormMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "FormMain";
+            this.Text = "Client";
             this.Load += new System.EventHandler(this.FormMain_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -170,10 +155,9 @@ namespace Client
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Panel indicator;
         private System.Windows.Forms.TextBox input;
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.Button btnConnect;
-        private System.Windows.Forms.Panel indicator;
     }
 }
--- Client/FormMain.cs
+++ Client/FormMain.cs
@@ -3,12 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.IO;
 using System.Linq;
-using System.Net.Sockets;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,17 +12,12 @@ namespace Client
 {
     public partial class FormMain : Form
     {
-        public TcpClient clientSocket; //client connection for TCP network services
-        public NetworkStream serverStream = default(NetworkStream); //Provides the underlying data flow for network access
-        string readData = null;
-        Thread ctThread;
-        String name = null;
         public FormMain()
         {
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
@@ -36,67 +27,9 @@ namespace Client
 
         }
 
-        private void btnSend_Click(object sender, EventArgs e)
+        private void textBox1_TextChanged(object sender, EventArgs e)
         {
-        }
 
-        //this function allows you to connect to the server
-        private void btnConnect_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                clientSocket.Connect("127.0.0.1", 5000);
-                readData = "Connected to Server ";
-                msg();
-
-                serverStream = clientSocket.GetStream();
-
-                byte[] outStream = Encoding.ASCII.GetBytes(name + "$");
-                serverStream.Write(outStream, 0, outStream.Length);
-                serverStream.Flush();
-                btnConnect.Enabled = false;
-
-                ctThread = new Thread(getMessage);
-                ctThread.Start();
-            }
-            catch (Exception er)
-            {
-                MessageBox.Show("Server Not Started");
-            }
-
-        }
-        private void getMessage()
-        {
-        }
-        //this function allows to display message on TextBox
-        private void msg()
-        {
-            if (this.InvokeRequired)
-                this.Invoke(new MethodInvoker(msg));
-            else
-                history.Text = history.Text + Environment.NewLine + " >> " + readData;
-        }
-        //converting object to ByteArray
-        public byte[] ObjectToByteArray(object _Object)
-        {
-            using (var stream = new MemoryStream())
-            {
-                var formatter = new BinaryFormatter();
-                formatter.Serialize(stream, _Object);
-                return stream.ToArray();
-            }
-        }
-        //converting ByteArray to object
-        public Object ByteArrayToObject(byte[] arrBytes)
-        {
-            using (var memStream = new MemoryStream())
-            {
-                var binForm = new BinaryFormatter();
-                memStream.Write(arrBytes, 0, arrBytes.Length);
-                memStream.Seek(0, SeekOrigin.Begin);
-                var obj = binForm.Deserialize(memStream);
-                return obj;
-            }
         }
     }
 }
